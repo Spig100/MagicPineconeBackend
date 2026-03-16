71d57b19f3ef86e1c6efdb8fad448f60366e9d8a
Shawn Lin <spig100.roc@gmail.com>
2026-03-17 00:10:28 +0800

    feat: Reserve a API schema placeholder file

---
+++ schemas/course_schema.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class CourseQuery(BaseModel):
+    # TODO: Making sure what UI/UX would like to query in this API
+    # Note that since we should have some situation that we
+    pass
