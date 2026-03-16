4fb242422b95924f3760da825d77aba257e09169
Shawn Lin <spig100.roc@gmail.com>
2026-03-16 23:23:54 +0800

    doc: Modify the RESTful API Doc's info

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 from routers import test
 
-app = FastAPI()
+app = FastAPI(
+    title='Magic Pinecone Backend API',
+    description='A project allows NCUers to retrieval the massive campus information in this single platform.',
+    version='0.0.0',
+    contact={
+        'name': 'Shawn Lin',
+        'email': 'spig100.roc@gmail.com'
+    }
+)
 
 app.include_router(test.router)
 
